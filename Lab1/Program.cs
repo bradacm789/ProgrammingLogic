2abf54dbe9b132bc3c14ef4aab16d553367462f8
bradacm789 <michaelbradac@att.net>
2025-03-19 13:55:16 +0000

    Create Lab1

---
+++ Lab1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Lab1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+   //Step 1 - Basic Output
+      Console.WriteLine("Michael Bradac Programming Logic");
+
+   //Step 2 - Use Comments
+      /*Name: Michael Bradac
+      Title: IT-1050 - Lab 1*/
+
+   //Step 3 - Declare and Use Variables
+      int favoriteNumber = 789;
+      string favoriteProgrammingLanguage = "R";
+      double numberPrograms = 50;
+      bool programmingExperience = true;
+  
+      Console.WriteLine("My favorite number is " + favoriteNumber);
+      Console.WriteLine("My favorite programming language is " + favoriteProgrammingLanguage);
+      Console.WriteLine("The number of programs I have written before this is probaly at least " + numberPrograms);
+      Console.WriteLine("It is " + programmingExperience + " that I have programming experience");
+
+    //Step 4 - there was no step 4 in the Lab Instructions
+  
+    //Step 5
+       const string my_school = "Tri-C";
+       Console.WriteLine("My school is " + my_school);
+
+    //Step 6 Identifiers are meaningful
+  
+    }
+}
+
