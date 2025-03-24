91d8f5a525ef9fc9d525d90f09265e348a5bd072
bradacm789 <michaelbradac@att.net>
2025-03-24 20:19:09 +0000

    Lab 2 First

--- Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7a59b11e42bd8df220e10eee19d89e026d42ca7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2abf54dbe9b132bc3c14ef4aab16d553367462f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Lab2/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Lab2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+     //Step 1 - Variable Declarations
+      int a = 59;
+      double y = 234.56D;
+      float x = 1.354F;
+      char myLetter = 'Y';
+      bool myBool = false;
+      string myString = "Here is a string";
+  
+      Console.WriteLine("a = " + a);
+      Console.WriteLine("y = " + y);
+      Console.WriteLine("x = " + x);
+      Console.WriteLine("The letter is " + myLetter);
+      Console.WriteLine("It is " + myLetter);
+      Console.WriteLine(myString);
+
+   //Step 2 - Type Casting
+      double y = 9.78D;
+      int myInt = (int) y;
+      Console.WriteLine(myInt);
+
+      int a = 59;
+      Console.WriteLine("String: " + Convert.ToString(a));
+
+      bool myBool2 = true;
+      Console.WriteLine("String: " + Convert.ToString(myBool2));
+
+   //Step 3 - Declare and Use Variables
+      Console.WriteLine("Enter your name ");
+      string myName = Console.ReadLine();
+      Console.WriteLine("Enter your age ");
+      int age = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("Hello " + myName +" happy " + age + " years of life.");
+}
