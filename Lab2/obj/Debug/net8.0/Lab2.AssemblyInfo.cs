4514c23b44cde3be56025c153df3832ba3573359
bradacm789 <michaelbradac@att.net>
2025-04-01 21:08:16 +0000

    Lab 3 Part 1

--- Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
+++ Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+333c7dcb5bc19538644c6a0f899ff68d1f4f2468")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a060ad2a002e053dddff78cc97a99ede1b723531")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Lab3/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Lab3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Part 1 - Operator Practice
+      int a = 10;
+      int b = 5;
+      int c = 15;
+  
+      Console.WriteLine("a > b: " + (a >b));
+      Console.WriteLine("a < c: " + (a < c));
+      Console.WriteLine("a > b and a > c: " + (a > b && a > c));
+      Console.WriteLine("a > b or a > c: " + (a > b || a > c));
+
+      //Part 2 - Boolean Logic
+
+      //Part 3 - Movie Ticket Pricing
+
+      //Part 4 - Using a Switch Statement
+    }
+}
