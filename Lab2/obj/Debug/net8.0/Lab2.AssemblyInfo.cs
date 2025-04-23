17e87863e3971aedf15015f3a477c350135b1c2d
bradacm789 <michaelbradac@att.net>
2025-04-23 22:11:07 +0000

    Lab 6 Prob 3

--- Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
+++ Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6b2a0cf89d602a8e4d2f24d26b7a39452a16b3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc6e70caa18e0972f41b41ba915e6a8c4bb2f16e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
