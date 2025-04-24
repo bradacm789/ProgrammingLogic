9fea8fc4b685e0049e3e1dcf5e740724a119ce60
bradacm789 <michaelbradac@att.net>
2025-04-24 12:41:14 +0000

    Lab 6

--- Lab6/obj/Debug/net8.0/Lab6.AssemblyInfo.cs
+++ Lab6/obj/Debug/net8.0/Lab6.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17e87863e3971aedf15015f3a477c350135b1c2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ceab32b8c859edb3d8e127ef45f3b3f5728cb55f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
