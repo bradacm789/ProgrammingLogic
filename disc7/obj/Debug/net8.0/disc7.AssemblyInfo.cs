316c9efd08eb853acc06de89aff3832fe2e0a39d
bradacm789 <michaelbradac@att.net>
2025-05-08 03:09:09 +0000

    Discussion 8

--- disc7/obj/Debug/net8.0/disc7.AssemblyInfo.cs
+++ disc7/obj/Debug/net8.0/disc7.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("disc7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d2de7df8df688580b1e02a2ff91cf88a7956632")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+282ca200fbde62ae1e00d88dbbbc670d8e9e608b")]
 [assembly: System.Reflection.AssemblyProductAttribute("disc7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("disc7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
